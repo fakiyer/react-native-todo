83094d559c0e13c292ed7a38b6a127ae5059eb89
fakiyer <fakiyer@gmail.com>
2017-12-12 21:51:12 +0900

    redux-persist 設定

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-// import {Provider} from 'react-redux';
-// import configureStore from './store/configureStore';
+import { Text, View } from 'react-native';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import { Provider } from 'react-redux';
+import configureStore from './src/store/configureStore';
 import Root from './src/Root';
+
+const { persistor, store } = configureStore();
+
+const onBeforeLift = () => {
+  // take some action before the gate lifts
+};
 
 export default class App extends React.Component {
   render() {
     return (
-      // <Provider store={store}>
-      <Root />
-      // </Provider>
+      <Provider store={store}>
+        <PersistGate
+          loading={
+            <View>
+              <Text>Loading...</Text>
+            </View>
+          }
+          onBeforeLift={onBeforeLift}
+          persistor={persistor}
+        >
+          <Root />
+        </PersistGate>
+      </Provider>
     );
   }
 }
---
+++ src/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore } from 'redux';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+import reducers from '../reducers';
+
+const config = {
+  key: 'root',
+  storage,
+};
+
+const reducer = persistCombineReducers(config, reducers);
+
+export default function configureStore() {
+  const store = createStore(reducer);
+  const persistor = persistStore(store);
+
+  return { persistor, store };
+}
