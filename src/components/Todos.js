8ab850291354ee2851e6b5755beb6468af0c421c
fakiyer <fakiyer@gmail.com>
2018-01-03 21:50:15 +0900

    コンポーネント分割

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import { Animated, FlatList, StyleSheet, TextInput, View } from 'react-native';
-import { CheckBox } from 'react-native-elements';
+import Todo from './Todo';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-  completeContainer: {
-    backgroundColor: '#f0f0f0',
-  },
-  completeText: {
-    color: '#c0c0c0',
-    textDecorationLine: 'line-through',
   },
 });
 
@@ -25,7 +13,6 @@ export default class Todos extends React.PureComponent {
   constructor(props) {
     super(props);
     this.onEndEditing = this.onEndEditing.bind(this);
-    this.onIconPress = this.onIconPress.bind(this);
     this.animatedValue = new Animated.Value(1);
   }
 
@@ -33,17 +20,6 @@ export default class Todos extends React.PureComponent {
     const { addTodo } = this.props;
 
     addTodo(e.nativeEvent.text);
-  }
-
-  onIconPress(id) {
-    const { completeTodo, deleteTodo } = this.props;
-
-    completeTodo(id);
-    Animated.timing(this.animatedValue, {
-      toValue: 0,
-      duration: 300,
-    }).start();
-    setTimeout(() => deleteTodo(id), 300);
   }
 
   render() {
@@ -59,18 +35,7 @@ export default class Todos extends React.PureComponent {
         <FlatList
           data={items.length > 0 ? items : undefined}
           keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <Animated.View style={{ opacity: this.animatedValue }}>
-              <CheckBox
-                title={item.text}
-                checked={item.completed}
-                checkedColor="gray"
-                containerStyle={item.completed ? styles.completeContainer : ''}
-                textStyle={item.completed ? styles.completeText : ''}
-                onIconPress={() => this.onIconPress(item.id)}
-              />
-            </Animated.View>
-          )}
+          renderItem={({ item }) => <Todo {...this.props} item={item} />}
         />
       </View>
     );
